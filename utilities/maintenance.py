a5c352e1bc23124a51d27fa9bc0841d9154a4092
harikrishna <harikrishna.ramagiri@yottatechports.com>
2023-03-14 16:17:54 +0530

    bucket

--- utilities/maintenance.py
+++ utilities/maintenance.py
@@ -2,10 +2,13 @@ import prefect
 from prefect import flow, get_run_logger
 from platform import node, platform, python_version
 from prefect.orion.api.server import ORION_API_VERSION as API
+from prefect.filesystems import S3
 
+s3_block = S3.load("my-block")
 
 @flow
 def maintenance():
+    print("this is maintenance")
     version = prefect.__version__
     logger = get_run_logger()
     logger.info("Network: %s. Instance: %s. Agent is healthy ✅️", node(), platform())
